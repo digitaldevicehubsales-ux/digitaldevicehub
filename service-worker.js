196dc39893969dfce5716552d441b45c1d7b6e16
digitaldevicehubsales-ux <digitaldevicehubsales@gmail.com>
2026-09-14 05:23:00 -0400

    Refresh cache for Supabase connection

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ddh-v2';
+const CACHE='ddh-v3';
 const ASSETS=['/','/index.html','/styles.css','/backend.css','/config.js','/app.js','/manifest.webmanifest','/favicon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
