32c3bb9d9172fe34cf65a8ec7e809e039e5f7635
digitaldevicehubsales-ux <digitaldevicehubsales@gmail.com>
2026-09-14 19:35:29 -0400

    Refresh auth flow cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ddh-v6';
+const CACHE='ddh-v7';
 const ASSETS=['/','/index.html','/styles.css','/backend.css','/config.js','/app.js','/auth-ui.js','/manifest.webmanifest','/favicon.svg','/resend-confirmation.html','/resend-confirmation.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
